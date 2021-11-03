6e7d7453ea7a019f09c16303db80062a2f8584f7
sonplusone <son.nguyen@plusonerobotics.com>
2021-11-02 23:27:25 -0500

    Update apollo link to use only web-socket

--- client/src/graphql/index.ts
+++ client/src/graphql/index.ts
@@ -1,13 +1,8 @@
-import { ApolloClient, ApolloLink } from "@apollo/client";
-import { cache } from "./cache";
-
-import { split, HttpLink } from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
+import { ApolloClient } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 
-const httpLink = new HttpLink({
-  uri: "http://localhost:5000/graphql",
-});
+// Cache
+import { cache } from "./cache";
 
 const wsLink = new WebSocketLink({
   uri: "ws://localhost:5000/graphql",
@@ -16,25 +11,8 @@ const wsLink = new WebSocketLink({
   },
 });
 
-// The split function takes three parameters:
-//
-// * A function that's called for each operation to execute
-// * The Link to use for an operation if the function returns a "truthy" value
-// * The Link to use for an operation if the function returns a "falsy" value
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  httpLink
-);
-
 const client = new ApolloClient({
-  link: ApolloLink.from([splitLink]),
+  link: wsLink,
   cache,
 });
 
